78c043a538565d79e89a6a1f712ba80994393412
hhtwfx <hhtwfx@126.com>
2016-02-25 16:37:23 +0800

    fixed bug

---
+++ kkt/messages/Mess.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace messages;
+
+
+/**
+ * @author Burt
+ * @version 1.0
+ * @created 25-¶þÔÂ-2016 16:35:03
+ */
+class Mess
+{
+
+	private $m_Id;
+
+	function __construct()
+	{
+	}
+
+	function __destruct()
+	{
+	}
+
+
+
+	/**
+		 * 
+		 * @param get_id
+		 */
+	public function getId(int $get_id)
+	{
+	}
+
+}
+?>
